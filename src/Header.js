651d48904e34aebab35aad4bd3f67d4e8cdcdfff
Allison Arnaud <arnaudallisonb@gmail.com>
2021-01-10 19:22:55 +1100

    first commit

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+
+export default class Header extends Component {
+
+
+
+    render() {
+
+        return (
+            <div style={{marginBottom: "20px", color: "white", width: "40%", margin: "10px auto 20px auto"}}>
+                <div style={{fontSize: "30px", textAlign: "center"}}>
+                    Allison's life story
+                </div>
+                <div style={{fontSize: "18px", textAlign: "center"}}>I am an aspiring software developer lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</div>
+            </div>
+        )
+    }
+
+    
+}
---
+++ src/Timeline.js
@@ -0,0 +1,141 @@
+import { Component } from 'react'
+import './Timeline.css';
+
+export default class Timeline extends Component {
+
+
+
+    render() {
+
+        return (
+            <div className="container">
+                {/* pre-line */}
+                <div className="filler"></div>
+                <div className="timelineHeader">START HERE</div>
+                <div className="filler">Jakarta 1994</div>
+                {/* first line */}
+                <div className="filler">
+                </div>
+                <div className="item">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* second line */}
+                <div className="filler">
+                </div>
+                <div className="item-a">Graduated from General Assembly Software Engineering Immersive Bootcamp, looking for my next great opportunity </div>
+                <div className="lineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* third line */}
+                <div className="filler">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* fourth line */}
+                <div className="filler">
+                </div>
+                <div className="item"></div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="lineRight">
+                </div>
+                <div className="item-b">Graduated from General Assembly Software Engineering Immersive Bootcamp, looking for my next great opportunity </div>
+                <div className="filler">
+                </div>
+                {/* fifth line */}
+                <div className="filler">
+                </div>
+                <div className="item">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="filler">
+                </div>
+
+
+                {/* sixth line */}
+                <div className="filler">
+                </div>
+                <div className="item">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* seventh line */}
+                <div className="filler">
+                </div>
+                <div className="item-c">Graduated from General Assembly Software Engineering Immersive Bootcamp, looking for my next great opportunity </div>
+                <div className="lineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* eighth line */}
+                <div className="filler">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="item">
+                </div>
+                <div className="filler">
+                </div>
+                {/* nineth line */}
+                <div className="filler">
+                </div>
+                <div className="item"></div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="lineRight">
+                </div>
+                <div className="item-d">Graduated from General Assembly Software Engineering Immersive Bootcamp, looking for my next great opportunity </div>
+                <div className="filler">
+                </div>
+                {/* tenth line */}
+                <div className="filler">
+                </div>
+                <div className="item">
+                </div>
+                <div className="straightLineLeft">
+                </div>
+                <div className="straightLineRight">
+                </div>
+                <div className="filler">
+                </div>
+            </div>
+            
+            
+        )
+    }
+
+    
+}
