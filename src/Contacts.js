17546c729a44072afde6b443e7f12ff6cebd7d24
Allison Arnaud <arnaudallisonb@gmail.com>
2021-01-29 01:10:09 +1100

    adding wording

--- src/Contacts.js
+++ src/Contacts.js
@@ -8,9 +8,9 @@ export default class Contacts extends Component {
         return (
             <div id="contact">
                 <h1 className="header">CONTACTS</h1>
-                <div style={{width: "800px", height:"150px", margin: "0 auto", backgroundColor: "white"}}>
-                    <h2>E-mail: arnaudallisonb@gmail.com</h2>
-                    <h2>Telephone: +61401668427</h2>
+                <div style={{width: "800px", height:"80px", margin: "0 auto", backgroundColor: "white"}}>
+                    <h3>E-mail: arnaudallisonb@gmail.com</h3>
+                    <h3>Telephone: +61401668427</h3>
                 </div>
                 
             </div>
--- src/Timeline.js
+++ src/Timeline.js
@@ -25,9 +25,9 @@ export default class Timeline extends Component {
                     </div>
                     <div className="item">
                     </div>
-                    <div style={{borderRight:"solid 3px blue"}}>
+                    <div style={{borderRight:"solid 3px #C5EBFE"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px blue"}}>
+                    <div style={{borderLeft:"solid 3px #C5EBFE", color: "#C5EBFE", fontSize:"20px", textAlign:"center"}}> 1994
                     </div>
                     <div className="item">
                     </div>
@@ -38,19 +38,19 @@ export default class Timeline extends Component {
                     </div>
                     <Accordion  className="item-a">
                         <Card>
-                            <Card.Header style={{ height:"50px", padding:"0"}}>
+                            <Card.Header style={{ height:"50px", padding:"0", backgroundColor:"#C5EBFE"}}>
                                 <Accordion.Toggle style={{color:"black", width:"100%", height:"100%"}} as={Button} variant="link" eventKey="1">
-                                    Childhood in Jakarta, Indonesia.
+                                    Early life
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
-                                <Card.Body>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
+                                <Card.Body style={{backgroundColor:"#C5EBFE"}}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Accordion>
-                    <div style={{borderBottom:"solid 6px blue", borderRight:"solid 3px blue"}}>
+                    <div style={{borderBottom:"solid 6px #C5EBFE", borderRight:"solid 3px #C5EBFE"}}> 
                     </div>
-                    <div style={{borderLeft:"solid 3px blue"}}>
+                    <div style={{borderLeft:"solid 3px #C5EBFE"}}>
                     </div>
                     <div className="item">
                     </div>
@@ -59,9 +59,9 @@ export default class Timeline extends Component {
                     {/* third line */}
                     <div className="filler">
                     </div>
-                    <div style={{borderRight:"solid 3px blue"}}>
+                    <div style={{borderRight:"solid 3px #C5EBFE"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px blue"}}>
+                    <div style={{borderLeft:"solid 3px #C5EBFE"}}>
                     </div>
                     <div className="item">
                     </div>
@@ -72,9 +72,9 @@ export default class Timeline extends Component {
                     </div>
                     <div className="item">
                     </div>
-                    <div style={{borderRight:"solid 3px red"}}>
+                    <div style={{borderRight:"solid 3px #FEC9A7", color:"#FEC9A7", fontSize:"20px", textAlign:"center"}}>2012
                     </div>
-                    <div style={{borderLeft:"solid 3px red"}}>
+                    <div style={{borderLeft:"solid 3px #FEC9A7"}}>
                     </div>
                     <div className="item">
                     </div>
@@ -84,19 +84,19 @@ export default class Timeline extends Component {
                     <div className="filler">
                     </div>
                     <div className="item"></div>
-                    <div style={{borderRight:"solid 3px red"}}>
+                    <div style={{borderRight:"solid 3px #FEC9A7"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px red", borderBottom:"solid 6px red"}}>
+                    <div style={{borderLeft:"solid 3px #FEC9A7", borderBottom:"solid 6px #FEC9A7"}}>
                     </div>
                     <Accordion  className="item-b">
                         <Card>
-                            <Card.Header style={{ height:"50px", padding:"0"}}>
+                            <Card.Header style={{ height:"50px", padding:"0", backgroundColor:"#FEC9A7"}}>
                                 <Accordion.Toggle style={{color:"black", width:"100%", height:"100%"}} as={Button} variant="link" eventKey="1">
-                                    Growing up loving technology and games.
+                                    Studied Electrical Engineering
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
-                                <Card.Body>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
+                                <Card.Body style={{backgroundColor:"#FEC9A7"}}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Accordion>
@@ -107,9 +107,9 @@ export default class Timeline extends Component {
                     </div>
                     <div className="item">
                     </div>
-                    <div style={{borderRight:"solid 3px red"}}>
+                    <div style={{borderRight:"solid 3px #FEC9A7"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px red"}}>
+                    <div style={{borderLeft:"solid 3px #FEC9A7"}}>
                     </div>
                     <div className="filler">
                     </div>
@@ -120,9 +120,9 @@ export default class Timeline extends Component {
                     </div>
                     <div className="item">
                     </div>
-                    <div style={{borderRight:"solid 3px #CCEECC"}}>
+                    <div style={{borderRight:"solid 3px #A5F8CE"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px #CCEECC"}}>
+                    <div style={{borderLeft:"solid 3px #A5F8CE", color:"#A5F8CE", fontSize:"20px", textAlign:"center"}}>2017
                     </div>
                     <div className="item">
                     </div>
@@ -133,19 +133,19 @@ export default class Timeline extends Component {
                     </div>
                     <Accordion  className="item-c">
                         <Card>
-                            <Card.Header style={{ height:"50px", padding:"0"}}>
+                            <Card.Header style={{ height:"50px", padding:"0", backgroundColor:"#A5F8CE"}}>
                                 <Accordion.Toggle style={{color:"black", width:"100%", height:"100%"}} as={Button} variant="link" eventKey="1">
-                                    Studying Engineering at University
+                                    Worked as an Electrical Engineer
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
-                                <Card.Body>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
+                                <Card.Body style={{backgroundColor:"#A5F8CE"}}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Accordion>
-                    <div style={{borderBottom:"solid 6px #CCEECC", borderRight:"solid 3px #CCEECC"}}>
+                    <div style={{borderBottom:"solid 6px #A5F8CE", borderRight:"solid 3px #A5F8CE"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px #CCEECC"}}>
+                    <div style={{borderLeft:"solid 3px #A5F8CE"}}>
                     </div>
                     <div className="item">
                     </div>
@@ -154,9 +154,9 @@ export default class Timeline extends Component {
                     {/* nineth line */}
                     <div className="filler">
                     </div>
-                    <div style={{borderRight:"solid 3px #CCEECC"}}>
+                    <div style={{borderRight:"solid 3px #A5F8CE"}}>
                     </div>
-                    <div style={{borderLeft:"solid 3px #CCEECC"}}>
+                    <div style={{borderLeft:"solid 3px #A5F8CE"}}>
                     </div>
                     <div className="item">
                     </div>
@@ -166,35 +166,73 @@ export default class Timeline extends Component {
                     <div className="filler">
                     </div>
                     <div className="item"></div>
-                    <div className="straightLineLeft">
+                    <div style={{borderRight:"solid 3px #FEFD97", color:"#FEFD97", fontSize:"20px", textAlign:"center"}}> 2020
                     </div>
-                    <div className="lineRight">
+                    <div style={{borderLeft:"solid 3px #FEFD97"}}>
+                    </div>
+                    <div className="item">
+                    </div>
+                    <div className="filler">
+                    </div>
+                    {/* eleventh */}
+                    <div className="filler">
+                    </div>
+                    <div className="item"></div>
+                    <div style={{borderRight:"solid 3px #FEFD97"}}>
+                    </div>
+                    <div style={{borderLeft:"solid 3px #FEFD97", borderBottom:"solid 6px #FEFD97"}}>
                     </div>
                     <Accordion  className="item-d">
                         <Card>
-                            <Card.Header style={{ height:"50px", padding:"0"}}>
+                            <Card.Header style={{ height:"50px", padding:"0", backgroundColor:"#FEFD97"}}>
                                 <Accordion.Toggle style={{color:"black", width:"100%", height:"100%"}} as={Button} variant="link" eventKey="1">
-                                    Started working as an engineer.
+                                    Studied Software Engineering
                                 </Accordion.Toggle>
                             </Card.Header>
                             <Accordion.Collapse eventKey="1">
-                                <Card.Body>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
+                                <Card.Body style={{backgroundColor:"#FEFD97"}}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
                             </Accordion.Collapse>
                         </Card>
                     </Accordion>
                     <div className="filler">
                     </div>
-                    {/* eleventh line */}
+                    {/* twelth line */}
                     <div className="filler">
                     </div>
                     <div className="item">
                     </div>
-                    <div className="straightLineLeft">
+                    <div style={{borderRight:"solid 3px #FEFD97"}}>
                     </div>
-                    <div className="straightLineRight">
+                    <div style={{borderLeft:"solid 3px #FEFD97"}}>
                     </div>
                     <div className="filler">
                     </div>
+                    {/* tirteenth line */}
+                    <div className="filler">
+                    </div>
+                    <div className="item">
+                    </div>
+                    <div style={{borderRight:"solid 3px #F197C0"}}>
+                    </div>
+                    <div style={{borderLeft:"solid 3px #F197C0", color:"#F197C0", fontSize:"20px", textAlign:"center"}}>2021
+                    </div>
+                    <div className="item">
+                    </div>
+                    <div className="filler">
+                    </div>
+                    {/* fourteenth line */}
+                    <Accordion  className="item-e">
+                        <Card>
+                            <Card.Header style={{ height:"50px", padding:"0", backgroundColor:"#F197C0"}}>
+                                <Accordion.Toggle style={{color:"black", width:"100%", height:"100%"}} as={Button} variant="link" eventKey="1">
+                                    Looking for my next great opportunity!
+                                </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body style={{backgroundColor:"#F197C0"}}>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
                 </div>
             </div>
             
